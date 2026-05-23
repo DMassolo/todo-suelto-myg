a2cae3d8e49429984b0988166e0c31f535a664f5
Damian <damianmassolo@gmail.com>
2026-05-23 15:41:19 -0300

    Agregar Service Worker para evitar caché en mobile

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'tmg-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
